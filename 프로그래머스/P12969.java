32b3ae4e393f0009ca70afa86db14808e32918dc
Yoon <dlstjr9390@gmail.com>
2023-11-29 09:44:25 +0900

    직사각형 별찍기

---
+++ 프로그래머스/P12969.java
@@ -0,0 +1,20 @@
+package 프로그래머스;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12969
+
+import java.util.Scanner;
+
+public class P12969 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
