a432978bf1f0148c8a79a3db94dabc6910bc3ad5
Yoon <dlstjr9390@gmail.com>
2023-11-29 09:22:37 +0900

    문자열 다루기 기본

---
+++ 프로그래머스/P12918.java
@@ -0,0 +1,30 @@
+package 프로그래머스;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12918
+
+public class P12918 {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "a123";
+        boolean result = sol.solution(s);
+
+        System.out.println(result); // "false"
+    }
+
+    static class Solution {
+        public boolean solution(String s) {
+            boolean answer = true;
+            if(s.length() == 4 || s.length() == 6){
+                for(int i=0; i<s.length(); i++){
+                    if(s.charAt(i) < '0' || s.charAt(i) > '9'){
+                        answer = false;
+                    }
+                }
+            } else{
+                answer = false;
+            }
+            return answer;
+        }
+    }
+}
