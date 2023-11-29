e6b773819a48a897c78e65e774a3da781a352c60
Yoon <dlstjr9390@gmail.com>
2023-11-29 09:10:16 +0900

    문자열 내림차순으로 배치하기

---
+++ 프로그래머스/P12917.java
@@ -0,0 +1,28 @@
+package 프로그래머스;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12917
+
+import java.util.Arrays;
+
+public class P12917 {
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String s = "Zbcdefg";
+        String result = sol.solution(s);
+
+        System.out.println(result); // "gfedcbZ"
+    }
+
+    static class Solution {
+        public String solution(String s) {
+            String answer = "";
+            char[] charArr = s.toCharArray();
+            Arrays.sort(charArr);
+            for(int i=charArr.length-1; i>=0; i--){
+                answer += charArr[i];
+            }
+            return answer;
+        }
+    }
+}
