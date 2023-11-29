446b55488e242c1031f12005eadf027ba4ddca3c
Yoon <dlstjr9390@gmail.com>
2023-11-29 09:32:46 +0900

    행렬의 덧셈

---
+++ 프로그래머스/P12950.java
@@ -0,0 +1,28 @@
+package 프로그래머스;
+
+// https://school.programmers.co.kr/learn/courses/30/lessons/12950
+
+import java.util.Arrays;
+
+public class P12950 {
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{1,2},{2,3}};
+        int[][] arr2 = {{3,4},{5,6}};
+
+        Solution sol = new Solution();
+        int[][] result = sol.solution(arr1, arr2);
+        System.out.println(Arrays.deepToString(result)); // "{{4,6},{7,9}}"
+    }
+    static class Solution {
+        public int[][] solution(int[][] arr1, int[][] arr2) {
+            int[][] answer = new int[arr1.length][arr1[0].length];
+            for(int i=0; i<arr1.length; i++){
+                for(int j=0; j<arr1[i].length; j++ ){
+                    answer[i][j] = arr1[i][j] + arr2[i][j];
+                }
+            }
+            return answer;
+        }
+    }
+}
