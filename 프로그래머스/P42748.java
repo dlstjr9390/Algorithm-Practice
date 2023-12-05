7f0efd5adfdd129c83457b6d2f7d201338c512fa
Yoon <dlstjr9390@gmail.com>
2023-12-05 10:01:07 +0900

    두 개 뽑아서 더하기

--- 프로그래머스/P42748.java
+++ 프로그래머스/P42748.java
@@ -1,39 +1,40 @@
 package 프로그래머스;
 
-// https://school.programmers.co.kr/learn/courses/30/lessons/42748
+// https://school.programmers.co.kr/learn/courses/30/lessons/68644
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class P42748 {
     public static void main(String[] args) {
 
-        int[] array = {1, 5, 2, 6, 3, 7, 4};
-        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
+        int[] numbers = {2, 1, 3, 4, 1};
 
         Solution sol = new Solution();
 
-        int[] result = sol.solution(array,commands);
-        System.out.println(Arrays.toString(result)); // {5, 6, 3}
+        int[] result = sol.solution(numbers);
+        System.out.println(Arrays.toString(result)); // {2, 3, 4, 5, 6, 7}
     }
 
     static class Solution {
-        public int[] solution(int[] array, int[][] commands) {
-            int[] answer = new int[commands.length];
-            List<Integer> intList = new ArrayList<>();
+        public int[] solution(int[] numbers) {
 
-            for(int i=0; i<commands.length; i++){
+            List<Integer> sumList = new ArrayList<>();
 
-                for(int j=commands[i][0]; j<=commands[i][1]; j++){
-                    intList.add(array[j-1]);
+            for(int i=0; i<numbers.length; i++){
+                for(int j=i+1; j<numbers.length; j++){
+                    if(!sumList.contains(numbers[i]+numbers[j])){
+                        sumList.add(numbers[i]+numbers[j]);
+                    }
                 }
-
-                Collections.sort(intList);
-                answer[i] = intList.get(commands[i][2]-1);
-                intList.clear();
             }
+
+            int[] answer = new int[sumList.size()];
+            for(int i=0; i<sumList.size(); i++){
+                answer[i] = sumList.get(i);
+            }
+            Arrays.sort(answer);
             return answer;
         }
     }
