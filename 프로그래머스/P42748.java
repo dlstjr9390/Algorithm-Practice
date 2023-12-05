f514ad9bb4607c1d9f36eb1a843e79752dc17dc4
Yoon <dlstjr9390@gmail.com>
2023-12-05 10:05:41 +0900

    K번째수

--- 프로그래머스/P42748.java
+++ 프로그래머스/P42748.java
@@ -1,40 +1,39 @@
 package 프로그래머스;
 
-// https://school.programmers.co.kr/learn/courses/30/lessons/68644
+// https://school.programmers.co.kr/learn/courses/30/lessons/42748
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class P42748 {
     public static void main(String[] args) {
 
-        int[] numbers = {2, 1, 3, 4, 1};
+        int[] array = {1, 5, 2, 6, 3, 7, 4};
+        int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
         Solution sol = new Solution();
 
-        int[] result = sol.solution(numbers);
-        System.out.println(Arrays.toString(result)); // {2, 3, 4, 5, 6, 7}
+        int[] result = sol.solution(array,commands);
+        System.out.println(Arrays.toString(result)); // {5, 6, 3}
     }
 
     static class Solution {
-        public int[] solution(int[] numbers) {
+        public int[] solution(int[] array, int[][] commands) {
+            int[] answer = new int[commands.length];
+            List<Integer> intList = new ArrayList<>();
 
-            List<Integer> sumList = new ArrayList<>();
+            for(int i=0; i<commands.length; i++){
 
-            for(int i=0; i<numbers.length; i++){
-                for(int j=i+1; j<numbers.length; j++){
-                    if(!sumList.contains(numbers[i]+numbers[j])){
-                        sumList.add(numbers[i]+numbers[j]);
-                    }
+                for(int j=commands[i][0]; j<=commands[i][1]; j++){
+                    intList.add(array[j-1]);
                 }
-            }
 
-            int[] answer = new int[sumList.size()];
-            for(int i=0; i<sumList.size(); i++){
-                answer[i] = sumList.get(i);
+                Collections.sort(intList);
+                answer[i] = intList.get(commands[i][2]-1);
+                intList.clear();
             }
-            Arrays.sort(answer);
             return answer;
         }
     }
