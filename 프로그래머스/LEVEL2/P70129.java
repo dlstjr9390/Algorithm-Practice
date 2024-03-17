809b04f410860ee223a249613459f4dafa0b6cf0
Yoon <dlstjr9390@gmail.com>
2024-03-17 23:04:43 +0900

    이진 변환 반복하기 풀이 중

---
+++ 프로그래머스/LEVEL2/P70129.java
@@ -0,0 +1,19 @@
+package 프로그래머스.LEVEL2;
+
+public class P70129 {
+
+  public static void main(String[] args) {
+    Solution sol = new Solution();
+    String s = "110010101001";
+
+    int[] result = sol.solution(s);
+
+    System.out.println(result);
+  }
+  static class Solution {
+    public int[] solution(String s) {
+      int[] answer = new int[2];
+      return answer;
+    }
+  }
+}
